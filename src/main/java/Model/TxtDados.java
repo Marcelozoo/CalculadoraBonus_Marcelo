a6ed057f641fbb0d29d2686611ca68c7a8a2d9eb
Marcelozoo <marcelo.pinheiro01@hotmail.com>
2023-04-30 15:54:52 -0300

    refatoracao

---
+++ src/main/java/Model/TxtDados.java
@@ -0,0 +1,42 @@
+package Model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+
+public class TxtDados implements IImportarDados {
+
+    @Override
+    public ArrayList<Integer> importarDados(File arquivo) {
+
+        ArrayList<Integer> dadosArquivo = new ArrayList();
+
+        try (Scanner scanner = new Scanner(arquivo)) {
+            while (scanner.hasNextLine()) {
+                String linha = scanner.nextLine();
+                int numero = Integer.parseInt(linha);
+                dadosArquivo.add(numero);
+            }
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo nao encontrado!");
+        }
+        return dadosArquivo;
+    }
+
+    @Override
+    public boolean verificaArquivo(JFileChooser fileChooser, int result) {
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            String extension = selectedFile.getName().substring(selectedFile.getName().lastIndexOf(".") + 1);
+            return extension.equals("txt");
+
+        }
+        return false;
+    }
+
+}
